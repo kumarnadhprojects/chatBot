4811616b25bb384756bc01a5d080ef086f4d71d8
kumarnadhprojects <83106603+kumarnadhprojects@users.noreply.github.com>
2021-06-27 11:47:08 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,3 +22,26 @@ sendBtn.onclick = function(){
         errorMsg.textContent = "Please enter valid input";
     }
 };
+
+function getMsg(event){
+    if (event.key === "Enter"){
+        let userValue = userInput.value;
+        if(userValue !== ""){
+            userInput.value = "";
+            errorMsg.textContent = "";
+            let para = document.createElement("p");
+            para.classList.add("text-right","mb-4");
+            messageContainer.appendChild(para);
+            
+            let messageCont = document.createElement("span");
+            messageCont.classList.add("msg-bg");
+            messageCont.textContent = userValue;
+            para.appendChild(messageCont);
+        }
+        else{
+            errorMsg.textContent = "Please enter valid input";
+        }
+    }
+}
+
+userInput.addEventListener("keydown", getMsg);
